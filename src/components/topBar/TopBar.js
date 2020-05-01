2d03761f4a18cd44ed6a18d9117e91d5da6a6ef7
Subhan Ahmed <sahmed@seven.com.au>
2020-05-01 14:01:51 +1000

    remove common properties from Topbar

--- src/components/topBar/TopBar.js
+++ src/components/topBar/TopBar.js
@@ -3,12 +3,9 @@ import styled from 'styled-components';
 import Bar from '../shared/Bar';
 
 const Container = styled(Bar)`
-  display: flex;
   justify-content: flex-start;
   height: 2em;
-  color: white;
   background: ${({ theme }) => theme.secondary.dark};
-  align-items: center;
   padding: 1.5em 8em;
   box-shadow: 0 4px 2px -1px rgb(65, 65, 65, 0.3);
 `;
