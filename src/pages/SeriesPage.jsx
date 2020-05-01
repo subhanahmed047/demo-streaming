094d3d8f15851fde85667e699a56c040eefa05f4
Subhan Ahmed <sahmed@seven.com.au>
2020-05-01 16:04:31 +1000

    add dummy cards to pages

--- src/pages/SeriesPage.jsx
+++ src/pages/SeriesPage.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Content from '../components/content';
+import TopBar from '../components/topBar';
+import { CardContainer, Card } from '../components/card';
 
 const SeriesPage = () => (
-  <Content>
-    <h1>Series PAGE WORKS!</h1>
-  </Content>
+  <>
+    <TopBar title="Popular Titles" />
+    <Content>
+      <CardContainer>
+        <Card
+          title="Popular Series"
+          linkTo="/"
+          img="https://streamcoimg-a.akamaihd.net/000/128/61/12861-PosterArt-ec32a81986a45eac7e080112075ab466.jpg"
+        ></Card>
+      </CardContainer>
+    </Content>
+  </>
 );
 
 export default SeriesPage;
